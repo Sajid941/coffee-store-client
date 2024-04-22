d94866c06433576fbd6264bbff60a650554c530b
sajid islam <sajidisalm941@gmail.com>
2024-04-22 11:45:38 +0600

    added banner section

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+const Banner = () => {
+    return (
+        <div className="bg-[url('https://i.postimg.cc/s20kYwfQ/Rectangle-2.png')] bg-no-repeat bg-cover lg:py-72 lg:px-48">
+            <div className="text-white  float-end space-y-3">
+                <h1 className="text-5xl">Would you like a Cup of Delicious Coffee?</h1>
+                <p>It,s coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of <br /> every moment!!! Enjoy the beautiful moments and make them memorable.</p>
+                <button className="bg-[#E3B577] btn text-2xl border-[#E3B577] rounded-none hover:text-white hover:bg-transparent   hover:border-white">Learn more</button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <img className="w-full h-16" src="https://i.postimg.cc/XN58LQ7M/Group-2.png" alt="" />
+        </div>
+    );
+};
+
+export default Header;
